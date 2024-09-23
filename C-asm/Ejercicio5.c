3c6aa90b54e44cff0230868dd978621c39bb9aa4
raztreuzz <raztreuzz1234@gmail.com>
2024-09-23 14:38:12 -0600

    main

---
+++ C-asm/Ejercicio5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    int e = 0;
+
+    asm("mov %5, %0\n"
+        "mov %6, %1\n"
+        "mov %7, %2\n"
+        "mov %8, %3\n"
+        "mov %9, %4\n"
+
+        : "=g"(a), "=g"(b), "=g"(c), "=g"(d), "=g"(e)
+        : "g"(1), "g"(2), "g"(3), "g"(4), "g"(5)
+        :);
+
+    printf("a: %d\n", a);
+    printf("b: %d\n", b);
+    printf("c: %d\n", c);
+    printf("d: %d\n", d);
+    printf("e: %d\n", e);
+
+    return 0;
+}
